b66121791c3a783c7d24cfaa5a55b15040350c7c
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-07-27 09:08:35 +0300

    Add files via upload

---
+++ 2_basic_animation/main.py
@@ -0,0 +1,32 @@
+import arcade
+import arcade.color
+import arcade.color
+
+WIDTH = 800
+HEIGHT = 600
+
+class GameWindow(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.WHITE)
+
+        self.timer = 0
+
+    
+    def on_draww(self):
+        """ Called  automatically about 60 times a second to draww objects"""
+        arcade.start_render()
+        
+        arcade.draw_text(f"{self.timer:.2f}", WIDTH/2, HEIGHT/2, arcade.Color.BLACK, 40)
+
+    def on_update(self, delta_time):
+        """ Called  automatically about 60 times a second to update objects"""
+        self.timer += delta_time
+
+def main():
+    """ Main method """
+    window = GameWindow(WIDTH,HEIGHT,"Basic Animation")
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
